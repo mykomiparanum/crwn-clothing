f1b9cae6fce42d5b976d9c6eac3ccc9200e69b05
Myko <m.miparanum@gmail.com>
2019-08-20 09:13:14 +0800

    Updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -26,9 +26,7 @@ const Header = ({ currentUser, hidden }) => (
             }
             <CartIcon />
         </div>
-        {
-            hidden ? null : <CartDropdown />
-        }
+        { hidden ? null : <CartDropdown /> }
         
     </div>
 );
